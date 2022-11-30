5c703b66cca14860c1ed93ca954886d02b35a50b
zamukaev <abu.safiia2016@mail.com>
2022-11-30 14:21:24 +0100

    cimmit2

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 
 app.use(cors());
 app.use(fileUpload({}));
-app.use(express.static("./static"));
+app.use(express.static("./src/static"));
 app.use('/api', router);
 app.use('/auth', authRouter);
 
